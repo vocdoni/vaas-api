a8f71fe3b78a5e7202b17fd620d40b216fba65f5
emmdim <emmdim@users.noreply.github.com>
2021-12-14 13:33:50 +0100

    Various DB call fixes and tests

--- test/testpgsql/pgsql_organization_test.go
+++ test/testpgsql/pgsql_organization_test.go
@@ -25,14 +25,20 @@ func TestOrganization(t *testing.T) {
 	c.Assert(int(id), qt.Not(qt.Equals), 0)
 	organizations[0].ID = id
 
-	count, err := API.DB.UpdateOrganization(integrators[0].SecretApiKey, organizations[0].EthAddress, "", "")
-	c.Assert(err, qt.IsNil)
-	c.Assert(int(id), qt.Not(qt.Equals), 0)
-
 	organization, err := API.DB.GetOrganization(integrators[0].SecretApiKey, organizations[0].EthAddress)
 	log.Infof("%w", organization)
 	c.Assert(err, qt.IsNil)
-	c.Assert(fmt.Sprintf("%x", organization.EthPrivKeyCicpher), qt.DeepEquals, fmt.Sprintf("%x", organizations[0].EthPrivKeyCicpher))
+	c.Assert(fmt.Sprintf("%x", organization.EthAddress), qt.Equals, fmt.Sprintf("%x", organizations[0].EthAddress))
+	c.Assert(fmt.Sprintf("%x", organization.EthPrivKeyCicpher), qt.Equals, fmt.Sprintf("%x", organizations[0].EthPrivKeyCicpher))
+	c.Assert(organization.QuotaPlanID, qt.Equals, organizations[0].QuotaPlanID)
+	c.Assert(organization.PublicAPIQuota, qt.Equals, organizations[0].PublicAPIQuota)
+	c.Assert(organization.PublicAPIToken, qt.Equals, organizations[0].PublicAPIToken)
+	c.Assert(organization.HeaderURI, qt.Equals, organizations[0].HeaderURI)
+	c.Assert(organization.AvatarURI, qt.Equals, organizations[0].AvatarURI)
+
+	count, err := API.DB.UpdateOrganization(integrators[0].SecretApiKey, organizations[0].EthAddress, "header", "avatar")
+	c.Assert(err, qt.IsNil)
+	c.Assert(int(id), qt.Not(qt.Equals), 0)
 
 	count, err = API.DB.CountOrganizations(integrators[0].SecretApiKey)
 	c.Assert(err, qt.IsNil)
@@ -41,6 +47,72 @@ func TestOrganization(t *testing.T) {
 	dbOrganizations, err := API.DB.ListOrganizations(integrators[0].SecretApiKey, nil)
 	c.Assert(err, qt.IsNil)
 	c.Assert(len(dbOrganizations), qt.Equals, 1)
+
+	// cleaning up (cascade delete from integrators)
+	for _, integrator := range integrators {
+		if err := API.DB.DeleteIntegrator(integrator.ID); err != nil {
+			t.Errorf("error deleting test entity: %w", err)
+		}
+
+	}
+}
+
+func TestOrganizationUpdate(t *testing.T) {
+	c := qt.New(t)
+	integrators := testcommon.CreateIntegrators(1)
+	var err error
+	integrators[0].ID, err = API.DB.CreateIntegrator(integrators[0].SecretApiKey, integrators[0].CspPubKey,
+		integrators[0].CspUrlPrefix, integrators[0].Name, integrators[0].Email)
+	c.Assert(err, qt.IsNil)
+
+	organizations := testcommon.CreateOrganizations(1)
+	id, err := API.DB.CreateOrganization(integrators[0].SecretApiKey, organizations[0].EthAddress,
+		organizations[0].EthPrivKeyCicpher, organizations[0].QuotaPlanID, organizations[0].PublicAPIQuota,
+		organizations[0].PublicAPIToken, organizations[0].HeaderURI, organizations[0].AvatarURI)
+	c.Assert(err, qt.IsNil)
+	c.Assert(int(id), qt.Not(qt.Equals), 0)
+	organizations[0].ID = id
+
+	count, err := API.DB.UpdateOrganization(integrators[0].SecretApiKey, organizations[0].EthAddress, "header", "avatar")
+	c.Assert(err, qt.IsNil)
+	c.Assert(count, qt.Equals, 1)
+	organization, err := API.DB.GetOrganization(integrators[0].SecretApiKey, organizations[0].EthAddress)
+	c.Assert(err, qt.IsNil)
+	c.Assert(fmt.Sprintf("%x", organization.EthAddress), qt.Equals, fmt.Sprintf("%x", organizations[0].EthAddress))
+	c.Assert(fmt.Sprintf("%x", organization.EthPrivKeyCicpher), qt.Equals, fmt.Sprintf("%x", organizations[0].EthPrivKeyCicpher))
+	c.Assert(organization.QuotaPlanID, qt.Equals, organizations[0].QuotaPlanID)
+	c.Assert(organization.PublicAPIQuota, qt.Equals, organizations[0].PublicAPIQuota)
+	c.Assert(organization.PublicAPIToken, qt.Equals, organizations[0].PublicAPIToken)
+	c.Assert(organization.HeaderURI, qt.Equals, "header")
+	c.Assert(organization.AvatarURI, qt.Equals, "avatar")
+
+	count, err = API.DB.UpdateOrganizationEthPrivKeyCipher(integrators[0].SecretApiKey, organizations[0].EthAddress, []byte("bb"))
+	c.Assert(err, qt.IsNil)
+	c.Assert(count, qt.Equals, 1)
+	organization, err = API.DB.GetOrganization(integrators[0].SecretApiKey, organizations[0].EthAddress)
+	c.Assert(err, qt.IsNil)
+	c.Assert(fmt.Sprintf("%x", organization.EthPrivKeyCicpher), qt.Equals, fmt.Sprintf("%x", []byte("bb")))
+	c.Assert(fmt.Sprintf("%x", organization.EthAddress), qt.Equals, fmt.Sprintf("%x", organizations[0].EthAddress))
+	c.Assert(organization.QuotaPlanID, qt.Equals, organizations[0].QuotaPlanID)
+	c.Assert(organization.PublicAPIQuota, qt.Equals, organizations[0].PublicAPIQuota)
+	c.Assert(organization.PublicAPIToken, qt.Equals, organizations[0].PublicAPIToken)
+	c.Assert(organization.HeaderURI, qt.Equals, "header")
+	c.Assert(organization.AvatarURI, qt.Equals, "avatar")
+
+	count, err = API.DB.UpdateOrganizationPublicAPIToken(integrators[0].SecretApiKey, organizations[0].EthAddress, "bb")
+	c.Assert(err, qt.IsNil)
+	c.Assert(count, qt.Equals, 1)
+	organization, err = API.DB.GetOrganization(integrators[0].SecretApiKey, organizations[0].EthAddress)
+	c.Assert(err, qt.IsNil)
+	c.Assert(organization.PublicAPIToken, qt.Equals, "bb")
+	c.Assert(fmt.Sprintf("%x", organization.EthPrivKeyCicpher), qt.Equals, fmt.Sprintf("%x", []byte("bb")))
+	c.Assert(fmt.Sprintf("%x", organization.EthAddress), qt.Equals, fmt.Sprintf("%x", organizations[0].EthAddress))
+	c.Assert(organization.QuotaPlanID, qt.Equals, organizations[0].QuotaPlanID)
+	c.Assert(organization.PublicAPIQuota, qt.Equals, organizations[0].PublicAPIQuota)
+	c.Assert(organization.AvatarURI, qt.Equals, "avatar")
+	c.Assert(organization.AvatarURI, qt.Equals, "avatar")
+
+	// count, err = API.DB.UpdateOrganizationPlan(integratorAPIKey []byte, ethAddress []byte, planID uuid.NullUUID, apiQuota int)
 
 	// cleaning up (cascade delete from integrators)
 	for _, integrator := range integrators {
