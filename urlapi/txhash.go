0b33e37109c30f8d95c866b66b53b7bf02111e32
NateWilliams2 <Williams2@grinnell.edu>
2021-12-20 10:15:41 -0600

    Add mined struct response

--- urlapi/txhash.go
+++ urlapi/txhash.go
@@ -43,6 +43,10 @@ type createElectionQuery struct {
 	hiddenResults     bool
 }
 
+type APIMined struct {
+	Mined bool `json:"mined,omitempty"`
+}
+
 // GET https://server/v1/priv/transactions/<transactionHash>
 func (u *URLAPI) getTxStatusHandler(msg *bearerstdapi.BearerStandardAPIdata,
 	ctx *httprouter.HTTPContext) error {
@@ -59,13 +63,13 @@ func (u *URLAPI) getTxStatusHandler(msg *bearerstdapi.BearerStandardAPIdata,
 	// TODO make vocclient api request to get tx status
 	// If tx has been mined, check dbTransactions map for pending db queries
 	if !mined {
-		return sendResponse(mined, ctx)
+		return sendResponse(APIMined{Mined: mined}, ctx)
 	}
 	tx, ok := u.dbTransactions.LoadAndDelete(hex.EncodeToString(txHash))
 	// If transaction not in map, it is a transaction
 	//  not associated with a db query (setProcessStatus)
 	if !ok {
-		return sendResponse(mined, ctx)
+		return sendResponse(APIMined{Mined: mined}, ctx)
 	}
 	switch queryTx := tx.(type) {
 	// Make the db request depending on query type
@@ -88,5 +92,5 @@ func (u *URLAPI) getTxStatusHandler(msg *bearerstdapi.BearerStandardAPIdata,
 			return fmt.Errorf("could not create election: %w", err)
 		}
 	}
-	return sendResponse(mined, ctx)
+	return sendResponse(APIMined{Mined: mined}, ctx)
 }
